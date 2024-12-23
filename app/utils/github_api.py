5c444c2decfdabd8acea32245e09f402215bd444
Otuekong <myshield02@gmail.com>
2024-12-23 21:52:11 +0100

    modified function to take user url instead of username

--- app/utils/github_api.py
+++ app/utils/github_api.py
@@ -1,6 +1,7 @@
 from typing import List
 import requests
 
+userUrl = 'https://github.com/username'
 
 class GitHubApi:
     """A utility for interacting with GitHub API."""
@@ -10,8 +11,10 @@ class GitHubApi:
         """Class constructor."""
         self.headers = {"Authorization": f"token {token}"}
 
-    async def fetch_repositories(self, username: str) -> List[dict]:
+    async def fetch_repositories(self, userUrl: str) -> List[dict]:
         """Fetch repositories by username."""
+        username = userUrl.split('/')[-1]
+
         url = f"{self.BASE_URL}/users/{username}/repos"
         response = requests.get(url, headers=self.headers)
         response.raise_for_status()
